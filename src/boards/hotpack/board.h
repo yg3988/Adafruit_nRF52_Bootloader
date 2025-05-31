e10b1ff50f48760ce21c3b421ae1fd96cf48aa29
Moon Yun-gi <yg3988@gmail.com>
2025-05-31 22:24:48 +0900

    remove not my boards

--- src/boards/hotpack/board.h
+++ src/boards/hotpack/board.h
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2018 Ha Thach for Adafruit Industries
+ * Copyright (c) 2022 Kyle McCreery
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -22,43 +22,44 @@
  * THE SOFTWARE.
  */
 
-#ifndef _ARDUINOBLE33_NRF52840_H
-#define _ARDUINOBLE33_NRF52840_H
+#ifndef HOTP4CK_H
+#define HOTP4CK_H
 
 #define _PINNUM(port, pin)    ((port)*32 + (pin))
+
+#define UICR_REGOUT0_VALUE UICR_REGOUT0_VOUT_3V3
 
 /*------------------------------------------------------------------*/
 /* LED
  *------------------------------------------------------------------*/
-#define LEDS_NUMBER           1  // red LED
-#define LED_PRIMARY_PIN       _PINNUM(0, 24)
-#define LED_STATE_ON          0
+#define LEDS_NUMBER       1
+#define LED_PRIMARY_PIN   _PINNUM(0, 26) // Blue
+#define LED_STATE_ON      1
 
 /*------------------------------------------------------------------*/
 /* BUTTON
  *------------------------------------------------------------------*/
-#define BUTTONS_NUMBER        2
-#define BUTTON_1              _PINNUM(1, 11)  // D2 switch
-#define BUTTON_2              _PINNUM(1, 12)  // D3 switch
-#define BUTTON_PULL           NRF_GPIO_PIN_PULLUP
+#define BUTTONS_NUMBER    2  // none connected at all
+#define BUTTON_1          _PINNUM(0, 18)  // unusable: RESET
+#define BUTTON_2          _PINNUM(0, 21)  // no connection
+#define BUTTON_PULL       NRF_GPIO_PIN_PULLUP
 
 //--------------------------------------------------------------------+
 // BLE OTA
 //--------------------------------------------------------------------+
-#define BLEDIS_MANUFACTURER   "Arduino"
-#define BLEDIS_MODEL          "Nano 33 BLE"
+#define BLEDIS_MANUFACTURER  "Hotp4ck"
+#define BLEDIS_MODEL         "Hotp4ck"
 
 //--------------------------------------------------------------------+
 // USB
 //--------------------------------------------------------------------+
-#define USB_DESC_VID           0x239A
-#define USB_DESC_UF2_PID       0x0063
-#define USB_DESC_CDC_ONLY_PID  0x0064
+#define USB_DESC_VID           0x1209
+#define USB_DESC_UF2_PID       0x4850
+#define USB_DESC_CDC_ONLY_PID  0x4850
 
-//------------- UF2 -------------//
-#define UF2_PRODUCT_NAME   "Arduino Nano 33 BLE"
-#define UF2_VOLUME_LABEL   "NANO33BOOT"
-#define UF2_BOARD_ID       "nRF52840-Nano-33"
-#define UF2_INDEX_URL      "https://www.arduino.cc/"
+#define UF2_PRODUCT_NAME  "Hotp4ck"
+#define UF2_VOLUME_LABEL  "HOTP4CK"
+#define UF2_BOARD_ID      "nRF52840-Hotp4ck-C"
+#define UF2_INDEX_URL     "https://github.com/yg3988/Hotp4ck60"
 
-#endif // _ITSY_NRF52840_H
+#endif // _PillBug_H
